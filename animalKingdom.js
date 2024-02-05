bbf37081c97e24e6bb90532c94831b95db805964
Nofisat Hassan <hassannofisat017@gmail.com>
2024-02-05 02:22:45 +0100

    added changes

--- animalKingdom.js
+++ animalKingdom.js
@@ -1,95 +1,128 @@
-//Encapsulation
+//Encapsulation: defining a property and method in a class
 class Animal {
-  constructor(name, classification) {
+  constructor(name) {
     this.name = name;
-    this.classification = classification;
   }
 
-  //A method all animals perform
-  eat() {
-    console.log(`${this.name} eats`);
+  //A public method to access the property, Abstraction because all animals eat
+  eats() {
+    return `${this.constructor.name} eats.`;
   }
 }
 
-class Arthropoda extends Animal { //Inheritance from Animal
-  constructor(name, isColdBlooded) {
+class Arthropoda extends Animal {
+  //Inheritance: child class Arthropoda inherits from parent class Animal
+  constructor(name, isColdBlooded, hasBackbone) {
     super(name, "Arthropoda");
     this.isColdBlooded = isColdBlooded;
-    this.hasBackbone = false;
+    this.hasBackbone = hasBackbone;
   }
-  //A specific method to class Arthropoda
+  //Polymorphism: A specific method to class Arthropoda
   eats() {
-    console.log(`${this.name} eats `);
+    return `${this.constructor.name} eats plants, algae for the herbivores, other animals for the carnivores, and both plants and animals for the omnivores.`;
   }
 }
 
-class Fish extends Animal { //Inheritance from Animal
-  constructor(name, isColdBlooded) {
+class Fish extends Animal {
+  //Inheritance: child class Fish inherits from parent class Animal
+  constructor(name, isColdBlooded, hasBackbone) {
     super(name, "Fish");
     this.isColdBlooded = isColdBlooded;
-    this.hasBackbone = true;
+    this.hasBackbone = hasBackbone;
   }
-  //A specific method to class Fish
+  //Polymorphism: A specific method to class Fish
   eats() {
-    console.log(`${this.name} eats `);
+    return `${this.constructor.name} eats plants, algae, insects, invertebrates and even other fishes depending on its species.`;
   }
 }
 
-class Amphibia extends Animal { //Inheritance from Animal
-  constructor(name, isColdBlooded) {
+class Amphibia extends Animal {
+  //Inheritance: child class Amphibia inherits from parent class Animal
+  constructor(name, isColdBlooded, hasBackbone) {
     super(name, "Amphibia");
     this.isColdBlooded = isColdBlooded;
-    this.hasBackbone = true;
+    this.hasBackbone = hasBackbone;
   }
-  //A specific method to class Amphibia
+  //Polymorphism: A specific method to class Amphibia
   eats() {
-    console.log(`${this.name} eats `);
+    return `${this.constructor.name} eats invertebrates, such as insects and worms and some of the larger species eat small vertebrates like fish.`;
   }
 }
 
-class Reptile extends Animal { //Inheritance from Animal
-  constructor(name, isColdBlooded) {
+class Reptiles extends Animal {
+  //Inheritance: child class Reptiles inherits from parent class Animal
+  constructor(name, isColdBlooded, hasBackbone) {
     super(name, "Reptile");
     this.isColdBlooded = isColdBlooded;
-    this.hasBackbone = true;
+    this.hasBackbone = hasBackbone;
   }
-  //A specific method to class Reptile
+  //Polymorphism: A specific method to class Reptiles
   eats() {
-    console.log(`${this.name} eats `);
+    return `${this.constructor.name} eat insects, small mammals, birds and even other reptiles`;
   }
 }
 
-class Aves extends Animal { //Inheritance from Animal
-  constructor(name, isWarmBlooded) {
+class Aves extends Animal {
+  //Inheritance: child class Aves inherits from parent class Animal
+  constructor(name, isWarmBlooded, hasBackbone) {
     super(name, "Aves");
     this.isWarmBlooded = isWarmBlooded;
-    this.hasBackbone = true;
+    this.hasBackbone = hasBackbone;
   }
-  //A specific method to class Aves
+  //Polymorphism: A specific method to class Aves
   eats() {
-    console.log(`${this.name} eats `);
+    return `${this.constructor.name} eat a variety of foods depending on its species which include insects, plants, seeds, fruits, other animals, and fish.`;
   }
 }
 
-class Mammals extends Animal { //Inheritance from Animal
-  constructor(name, isWarmBlooded) {
+class Mammals extends Animal {
+  //Inheritance: child class Mammals inherits from parent class Animal
+  constructor(name, isWarmBlooded, hasBackbone) {
     super(name, "Mammals");
     this.isWarmBlooded = isWarmBlooded;
-    this.hasBackbone = true;
+    this.hasBackbone = hasBackbone;
   }
-  //A specific method to class Mammals
+  //Polymorphism: A specific method to class Mammals
   eats() {
-    console.log(`${this.name} eats `);
+    return `${this.constructor.name} eat plant, animals or both plants and animals`;
   }
 }
 
-//Abstraction
+//Abstraction: an higher level representation of the animals
 class AnimalKingdom {
   constructor() {
     this.animals = [];
   }
 
-  addAnAnimal(animal) {
+  addAnimal(animal) {
     this.animals.push(animal);
   }
 }
+
+//creating instances to run the methods
+const myAnimalKingdom = new AnimalKingdom();
+
+const myArthropoda = new Arthropoda("Buterfly", true, false);
+const myFish = new Fish("Tilapia fish", true, true);
+const myAmphibia = new Amphibia("Frog", true, true);
+const myReptile = new Reptiles("Tortoise", true, true);
+const myBird = new Aves("Eagle", true, true);
+const myMammal = new Mammals("Cat", true, true);
+
+console.log(myArthropoda.eats());
+console.log(myArthropoda);
+
+console.log(myFish.eats());
+console.log(myFish);
+
+console.log(myAmphibia.eats());
+console.log(myAmphibia);
+
+console.log(myReptile.eats());
+console.log(myReptile);
+
+console.log(myBird.eats());
+console.log(myBird);
+
+console.log(myMammal.eats());
+console.log(myMammal);
